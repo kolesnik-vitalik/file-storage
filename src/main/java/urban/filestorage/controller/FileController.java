2107bb645f32b17714d01af81a8dd0370aaa7c98
Kolesnik <kolesnik.vitalik21@gmail.com>
2025-10-05 00:32:33 +0300

    added code

--- src/main/java/urban/filestorage/controller/FileController.java
+++ src/main/java/urban/filestorage/controller/FileController.java
@@ -1,4 +1,40 @@
 package urban.filestorage.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import urban.filestorage.service.FileService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/files")
 public class FileController {
+    private final FileService fileService;
+
+    @PostMapping("/item")
+    public ResponseEntity<Boolean> uploadFile(@RequestParam("file") MultipartFile file) {
+        if(file.isEmpty()) {
+            return ResponseEntity.badRequest().body(false);
+        }
+        String path = "/" + file.getOriginalFilename();
+
+        try {
+            fileService.upload(path, file.getBytes());
+            return ResponseEntity.ok(true);
+        } catch (URISyntaxException | IOException e) {
+            log.error(e.getMessage());
+        }
+
+        return ResponseEntity.ok(false);
+    }
 }
--- src/main/java/urban/filestorage/service/FileService.java
+++ src/main/java/urban/filestorage/service/FileService.java
@@ -1,4 +1,22 @@
 package urban.filestorage.service;
 
+import com.github.sardine.Sardine;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.client.utils.URIBuilder;
+import org.springframework.stereotype.Service;
+import urban.filestorage.config.WebDavConfig;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@Service
+@RequiredArgsConstructor
 public class FileService {
+    private final Sardine sardine;
+    private final WebDavConfig webDavConfig;
+
+    public void upload(String path, byte[] content) throws URISyntaxException, IOException {
+        String fullPath = new URIBuilder(webDavConfig.getRoot()).setPath(path).toString();
+        sardine.put(fullPath, content);
+    }
 }
